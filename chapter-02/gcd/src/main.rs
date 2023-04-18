f1ce2a6a42078c09e3a01d8725bb14b6218a91e4
Henrik Samuelsson <henrik.samuelsson2@saabgroup.com>
2023-04-18 07:09:04 +0200

    Add function to calculate gcd.

--- chapter-02/gcd/src/main.rs
+++ chapter-02/gcd/src/main.rs
@@ -1,3 +1,19 @@
 fn main() {
-    println!("Hello, world!");
+    let a = 54;
+    let b = 24;
+    let g = gcd(a, b);
+    println!("Greatest common divisor of {} and {} is {}.", a, b, g);
+}
+
+fn gcd(mut n: u64, mut m: u64) -> u64 {
+    assert!(n != 0 && m != 0);
+    while m != 0 {
+        if m < n {
+            let t = m;
+            m = n;
+            n = t;
+        }
+        m = m % n;
+    }
+    n
 }
